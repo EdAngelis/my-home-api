5a2da96d882ff9e3c06bb1c404822b40a4394b2c
Ed Angelis <ed4ngelis@gmail.com>
2023-10-05 17:38:31 -0300

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,11 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['http://localhost:3000', 'https://my-home-front.vercel.app']
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
